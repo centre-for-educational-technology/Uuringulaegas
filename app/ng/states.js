f8d769498d1f5d08a9ef57639a89fead6a3e588b
sanderaido <aido@tlu.ee>
2016-03-30 19:10:28 +0300

    Redirected to login page if accessing forbidden routes while unauthenticated

--- app/ng/states.js
+++ app/ng/states.js
@@ -40,52 +40,62 @@ angular.module('arkofinquiryApp')
 
       .state('inq_act', {
         url: '/inq_act',
-        templateUrl: base + 'ng/components/main/views/parent_view.html'
+        templateUrl: base + 'ng/components/main/views/parent_view.html',
+        authenticate: true
       })
       .state('inq_act.main', {
         templateUrl: base + 'ng/components/inq_act/views/inq_act_parent_view.html',
-        controller: 'InquiryActivityParentCtrl'
+        controller: 'InquiryActivityParentCtrl',
+        authenticate: true
       })
       .state('inq_act.main.list', {
         url: "/list",
         templateUrl: base + 'ng/components/inq_act/views/inquiry_activity_list.html',
-        controller: 'InquiryActivityListCtrl'
+        controller: 'InquiryActivityListCtrl',
+        authenticate: true
       })
       .state('inq_act.main.new', {
         url: "/new",
         templateUrl: base + 'ng/components/inq_act/views/add_inquiry_activity_form.html',
-        controller: 'AddInquiryActivityCtrl'
+        controller: 'AddInquiryActivityCtrl',
+        authenticate: true
       })
       .state('inq_act.details', {
         url: "/:id",
         templateUrl: base + 'ng/components/inq_act/views/inq_act_detail_page.html',
-        controller: 'InquiryActivityDetailPageCtrl'
+        controller: 'InquiryActivityDetailPageCtrl',
+        authenticate: true
       })
       .state('inq_act.main.search', {
         url: '/search',
         abstract: true,
-        templateUrl: base + 'ng/components/main/views/parent_view.html'
+        templateUrl: base + 'ng/components/main/views/parent_view.html',
+        authenticate: true
       })
       .state('inq_act.main.search.tags', {
         url: "/tags",
         templateUrl: base + 'ng/components/inq_act/views/inq_act_tag_cloud.html',
-        controller: 'InquiryActivityTagCloudCtrl'
+        controller: 'InquiryActivityTagCloudCtrl',
+        authenticate: true
       })
       .state('inq_act.main.search.search', {
         url: "/search",
         templateUrl: base + 'ng/components/inq_act/views/inq_act_search_form.html',
-        controller: 'InquiryActivitySearchCtrl'
+        controller: 'InquiryActivitySearchCtrl',
+        authenticate: true
       })
 
 
       .state('user', {
         url: '/user',
-        templateUrl: base + 'ng/components/main/views/parent_view.html'
+        templateUrl: base + 'ng/components/main/views/parent_view.html',
+        authenticate: true
       })
       .state('user.list', {
         url: '/list',
         templateUrl: base + 'ng/components/user/views/user_list.html',
-        controller: 'UserListCtrl'
+        controller: 'UserListCtrl',
+        authenticate: true
       })
       .state('user.register', {
         url: '/register',
@@ -95,70 +105,92 @@ angular.module('arkofinquiryApp')
       .state('user.edit', {
         url: '/edit',
         templateUrl: base + 'ng/components/user/views/user_edit.html',
-        controller: 'UserEditCtrl'
+        controller: 'UserEditCtrl',
+        authenticate: true
       })
       .state('user.details', {
         url: '/:id',
         templateUrl: base + 'ng/components/user/views/user_passport.html',
-        controller: 'UserPassportCtrl'
+        controller: 'UserPassportCtrl',
+        authenticate: true
       })
       .state('welcome', {
         url: '/welcome',
         templateUrl: base + 'ng/components/user/views/user_survey.html',
-        controller: 'UserSurveyCtrl'
+        controller: 'UserSurveyCtrl',
+        authenticate: true
       })
 
 
       .state('group', {
         url: '/group',
-        templateUrl: base + 'ng/components/main/views/parent_view.html'
+        templateUrl: base + 'ng/components/main/views/parent_view.html',
+        authenticate: true
       })
       .state('group.main', {
         templateUrl: base + 'ng/components/group/views/group_parent_view.html',
-        controller: 'GroupParentCtrl'
+        controller: 'GroupParentCtrl',
+        authenticate: true
       })
       .state('group.main.list', {
         url: '/list',
         templateUrl: base + 'ng/components/group/views/group_list.html',
-        controller: 'GroupListCtrl'
+        controller: 'GroupListCtrl',
+        authenticate: true
       })
       .state('group.main.new', {
         url: '/new',
         templateUrl: base + 'ng/components/group/views/add_group.html',
-        controller: 'AddGroupCtrl'
+        controller: 'AddGroupCtrl',
+        authenticate: true
       })
       .state('group.main.hallOfFame', {
         url: '/halloffame',
         templateUrl: base + 'ng/components/group/views/hall_of_fame.html',
-        controller: 'HallOfFameCtrl'
+        controller: 'HallOfFameCtrl',
+        authenticate: true
       })
       .state('group.detail', {
         url: '/:id',
         abstract: false,
         templateUrl: base + 'ng/components/group/views/group_detail_page_parent.html',
-        controller: 'GroupParentCtrl'
+        controller: 'GroupParentCtrl',
+        authenticate: true
       })
       .state('group.detail.view', {
         url: '/view',
         templateUrl: base + 'ng/components/group/views/detail_page_views/group_page_info.html',
-        controller: 'GroupPageInfoCtrl'
+        controller: 'GroupPageInfoCtrl',
+        authenticate: true
       })
       .state('group.detail.comments', {
         url: '/comments',
         templateUrl: base + 'ng/components/group/views/detail_page_views/group_page_comments.html',
-        controller: 'GroupCommentsCtrl'
+        controller: 'GroupCommentsCtrl',
+        authenticate: true
       })
       .state('group.detail.edit', {
         url: '/edit',
         templateUrl: base + 'ng/components/group/views/detail_page_views/group_page_edit.html',
-        controller: 'EditGroupCtrl'
+        controller: 'EditGroupCtrl',
+        authenticate: true
       })
       .state('group.detail.act', {
         url: '/activities',
         templateUrl: base + 'ng/components/group/views/detail_page_views/group_page_activities.html',
-        controller: 'GroupPageInfoCtrl'
+        controller: 'GroupPageInfoCtrl',
+        authenticate: true
       });
 
 
     //$locationProvider.html5Mode(true);
+  })
+  .run(function ($rootScope, $state) {
+    $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams){
+      if (toState.authenticate && !$rootScope.loggedIn){
+        // User isn’t authenticated
+        $state.transitionTo("login");
+        event.preventDefault();
+      }
+    });
   });
